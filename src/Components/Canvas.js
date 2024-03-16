8d0008d28d8fdfccbc5af251d47c31323b05cc68
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-16 11:53:01 +0530

    project setup

---
+++ src/Components/Canvas.js
@@ -0,0 +1,9 @@
+const Canvas = ({ width, height }) => {
+  return <canvas width={width} height={height} style={canvasStyle} />;
+};
+
+export default Canvas;
+
+const canvasStyle = {
+  border: "1px solid black",
+};
