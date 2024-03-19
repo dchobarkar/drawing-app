c2fb69e4bc4b582ccdf425c30e0d23b8da9af1be
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-19 14:09:41 +0530

    add code for drawing on canvas

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -3,7 +3,12 @@ import { useOnDraw } from "./Hooks";
 const Canvas = ({ width, height }) => {
   const setCanvasRef = useOnDraw(onDraw);
 
-  function onDraw(ctx, point) {}
+  function onDraw(ctx, point) {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 
   return (
     <canvas
